f70c20ef368dc8005462caa2213e489ab1d8a0a6
Deli0MaN <trial_xperia10ii@protonmail.com>
2024-05-17 21:42:47 +0300

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh gtx_1050_ti_mobile.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh GeForce MX150 [GP108-A]-86.08.40.00.48.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
